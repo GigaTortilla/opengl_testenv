b23533e4231d9aa1f07139270aa32d0c8b165534
Martin Appel <martinappel01@gmail.com>
2024-03-01 23:06:51 +0100

    adapted for auto refreshing custom libs

--- build.bat
+++ build.bat
@@ -1,2 +1,9 @@
-gcc -o test_file .\src\*.c -I .\include -L .\lib -lglfw3dll -lcglm.dll -mwindows -Wall -Wextra
+gcc -c -o camdll.o .\src\camera\cam.c -D CAMDLL_EXPORTS -I .\include
+gcc -shared -o libcamdll.dll .\camdll.o -L .\lib -lglfw3dll
+del camdll.o
+gendef libcamdll.dll
+dlltool --def libcamdll.def --dllname libcamdll.dll --output-lib lib\libcamdll.a
+del libcamdll.def
+
+gcc -o test_file .\src\*.c -I .\include -L .\lib -lglfw3dll -lcglm.dll -lcamdll -mwindows -Wall -Wextra
 pause
