cd908d9253ed336ca6febd4970b218d4fb6efe80
GigaTortilla <martinappel01@gmail.com>
2024-03-05 14:39:22 +0100

    restructured texture loading

--- src/testenv_funcs.c
+++ src/testenv_funcs.c
@@ -123,22 +123,22 @@ unsigned int genBindTexClamp2Edge(char* imagePath)
 	int width, height, nrChannels;
 	glGenTextures(1, &texture);
 	
-	glBindTexture(GL_TEXTURE_2D, texture);
-	
-	// Texture parameter settings
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	
 	unsigned char *data = stbi_load(imagePath, &width, &height, &nrChannels, 0);
 	if(data)
 	{
+		glBindTexture(GL_TEXTURE_2D, texture);
+
 		if(nrChannels == 3)
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 		else if(nrChannels == 4)
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
+		
+		// Texture parameter settings
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	}
 	else 
 	{
